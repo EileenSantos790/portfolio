af7836e0d6f94ca28cc6eb2c7712fc690bae2ef1
Eileen <eileen.santos@outlook.de>
2025-11-10 13:59:40 +0100

    hero

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from "../shared/footer/footer.component";
-import { NavbarComponent } from "../navbar/navbar.component";
+import { NavbarComponent } from "../shared/navbar/navbar.component";
 import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
