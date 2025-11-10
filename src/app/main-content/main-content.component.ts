84a6e0740aa65d60523b579b4cef838dffd0a730
Eileen <eileen.santos@outlook.de>
2025-11-10 16:24:48 +0100

    Add About component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../shared/navbar/navbar.component";
 import { HeroComponent } from '../hero/hero.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
