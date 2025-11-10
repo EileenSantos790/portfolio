256a550388f089dfb09c004c14506fcdca958b82
Eileen <eileen.santos@outlook.de>
2025-11-10 09:04:09 +0100

    create hero, navbar, and footer components

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../shared/footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent, HeroComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
