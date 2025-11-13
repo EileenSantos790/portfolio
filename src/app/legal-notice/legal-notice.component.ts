44f146c1715c09b1f453bcfaa94529093a187808
Eileen <eileen.santos@outlook.de>
2025-11-13 11:29:17 +0100

    Add responsive header and mobile menu functionality to legal notice component

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { TranslationService, Language, Translations } from '../services/translation.service';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterLink],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
-export class LegalNoticeComponent {}
+export class LegalNoticeComponent {
+  isMenuOpen = false;
+  translations: Translations;
+  activeLanguage: Language = 'EN';
+
+  constructor(private translationService: TranslationService) {
+    this.activeLanguage = this.translationService.getCurrentLanguageValue();
+    this.translations = this.translationService.getTranslations();
+    
+    this.translationService.getCurrentLanguage().subscribe(lang => {
+      this.activeLanguage = lang;
+      this.translations = this.translationService.getTranslations();
+    });
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  setActiveLanguage(language: Language): void {
+    this.translationService.setLanguage(language);
+    this.closeMenu();
+  }
+}
